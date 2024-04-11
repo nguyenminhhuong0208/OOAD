66eae0d0ab16ee6c0df9cd5450bbb7a7077b8354
Minkduc2012 <166316217+Minkduc2012@users.noreply.github.com>
2024-04-11 12:31:44 +0700

    Add files via upload

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState } from 'react';
+import all_product from "../Components/Assets/all_product";
+
+
+export const ShopContext = createContext(null);
+const getDefaultCart = ()=>{
+    let cart = {};
+    for (let index = 0; index < all_product.length+1; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+}
+const ShopContextProvider = (props)=>{
+    // can access cartItems data in any component
+    const [cartItems,setCartItems] = useState(getDefaultCart());
+    
+    //console.log(cartItems);
+    const addToCart = (itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+        //console.log(cartItems);
+    }
+    const removeFromCart = (itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+    }
+
+    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;

f4db62e1f54561fc318aa7110e5ab21dcf2ae0f0
Final_project_22022542_Nguyen_Minh_Huong <nguyenminhhuong1222@gmail.com>
2024-04-17 00:07:03 +0700

    component change

--- fontend/src/Components/NewsLetter/NewsLetter.jsx
+++ fontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import './NewsLetter.css'
-export const NewsLetter = () => {
+import React from "react";
+import "./NewsLetter.css";
+const NewsLetter = () => {
   return (
-    <div className='newsletter'>
-        <h1>GET EXCLUSIVE OFFERS ON YOUR EMAIL</h1>
-        <p>Subcribe to our newletter and stay updated</p>
-        <div>
-            <input type='email' placeholder='Your Email id' /> 
-            <button>Subcribe</button>
-            
-        </div>
+    <div className="newsletter">
+      <h1>Get Exclusive Offers On Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type="email" placeholder="Your Email Id" />
+        <button>Subscribe</button>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
+
+export default NewsLetter;

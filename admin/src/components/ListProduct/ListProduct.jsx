582d50a380f5ca2d32ffe072f234ee853c797d43
hienvandam <22022664@vnu.end.vn>
2024-04-21 16:49:38 +0700

    1. Make admin/product list responsive 2. Make Add product function work

--- admin/src/components/ListProduct/ListProduct.jsx
+++ admin/src/components/ListProduct/ListProduct.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ListProduct.css'
-
+import cross_icon from'../../assets/cart_cross_icon.png'
 
 const ListProduct = () => {
+
+    const [allproducts, setAllProducts] = useState([]);
+
+    const fetchInfo = async ()=>{
+      await fetch('http://localhost:4000/allproducts')
+      .then((res)=>res.json())
+      .then((data)=>{setAllProducts(data)});
+    }
+
+    useEffect(()=>{
+      fetchInfo();
+    },[])
+
+    const remove_product = async (id)=>{
+      await fetch('http://localhost:4000/removeproduct', {
+        method: 'POST',
+        headers: {
+          Accept:'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({id:id})
+      })
+      await fetchInfo();
+    }
+
   return (
     <div className='list-product'>
-        
+        <h1>All Products list</h1>
+        <div className="listproduct-format-main">
+          <p>Products</p>
+          <p>Title</p>
+          <p>Old Price</p>
+          <p>New Price</p>
+          <p>Category</p>
+          <p>Remove</p>
+        </div>
+        <div className="listproduct-allproducts">
+          <hr />
+          {allproducts.map((product, index)=>{
+            return <><div key = {index} className="listproduct-format-main listproduct-format">
+              <img src={product.image} alt="" className="listproduct-product-icon" />
+              <p>{product.name}</p>
+              <p>${product.old_price}</p>
+              <p>${product.new_price}</p>
+              <p>{product.category}</p>
+              <img onClick = {()=>{remove_product(product.id)}} className = 'list-product-remove-icon' src={cross_icon} alt="" />
+            </div>
+            <hr />
+            </>
+          })}
+        </div>
     </div>
   )
 }

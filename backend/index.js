1f7689782e02c236e4218c6567e73956a9181e05
nhuyhoan004 <22022590@vnu.edu.vn>
2024-04-21 21:08:38 +0700

    update backend signup

--- backend/index.js
+++ backend/index.js
@@ -124,6 +124,55 @@ app.get('/allproducts',async(req,res)=>{
     res.send(products);
 })
 
+// Tạo Schema cho mô hình user
+
+const Users = mongoose.model("Users",{
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    cartData: {
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// Creating Endpoint for regestering the user
+app.post('/signup', async(req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if(check) {
+        return res.status(400).json({success:false,errors:"Email already exists"});
+    }
+    let cart = {};
+    for (let i = 0; i < 10; i++) {
+        cart[i] = 0;
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user: {
+            id: user.id,
+        },
+    };
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token});
+});
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server is running on port " +port);

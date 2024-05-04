6fddae2738b78458944f29f8259d7b4f4f381a2d
nhuyhoan004 <22022590@vnu.edu.vn>
2024-05-04 15:19:52 +0700

    update cmt

--- fontend/src/Components/Language-selector/language-selector.jsx
+++ fontend/src/Components/Language-selector/language-selector.jsx
@@ -8,19 +8,19 @@ const languages = [
 ];
 
 const LanguageSelector = () => {
-  const { i18n } = useTranslation();
-  const [showDropdown, setShowDropdown] = useState(false); // State to control dropdown visibility
+  const { i18n } = useTranslation(); // để truy cập vào đối tượng i18n của thư viện
+  const [showDropdown, setShowDropdown] = useState(false); // kiểm soát việc hiển thị/ẩn dropdown chọn ngôn ngữ.
 
   useEffect(() => {
-    document.body.dir = i18n.dir();
+    document.body.dir = i18n.dir(); // để thay đổi hướng viết của ứng dụng
   }, [i18n, i18n.language]);
 
   const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
+    i18n.changeLanguage(lng); // để thay đổi ngôn ngữ hiển thị của ứng dụng
   };
 
   const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
+    setShowDropdown(!showDropdown); // đóng/mở dropdown chọn ngôn ngữ.
   };
 
   return (

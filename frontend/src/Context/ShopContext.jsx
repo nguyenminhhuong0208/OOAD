d5c1d792d5b7a83a34beeacafea17f9eb77b1a21
nhuyhoan004 <22022590@vnu.edu.vn>
2024-05-25 13:27:14 +0700

    frontend

---
+++ frontend/src/Context/ShopContext.jsx
@@ -0,0 +1,105 @@
+import React, { createContext, useEffect, useState } from 'react';
+//import all_product from "../Components/Assets/all_product";
+//import CartItems from '../Components/CartItems/CartItems';
+
+
+export const ShopContext = createContext(null);
+const getDefaultCart = ()=>{
+    let cart = {};
+    for (let index = 0; index < 300+1; index++) { //đổi all_product.length thành 300
+        cart[index] = 0;
+    }
+    return cart;
+}
+const ShopContextProvider = (props)=>{
+    const [all_product,setAll_Product]= useState([]);
+    // can access cartItems data in any component
+    const [cartItems,setCartItems] = useState(getDefaultCart());
+
+    useEffect(()=>{
+        fetch('http://localhost:4000/allproducts') //load product từ backend
+        .then((response)=>response.json())
+        .then((data)=>setAll_Product(data))
+
+        if(localStorage.getItem('auth-token')){
+            fetch('http://localhost:4000/getcart',{
+                method:'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:"",
+            }).then((response)=>response.json())
+            .then((data)=>setCartItems(data));
+        }
+    },[])
+    
+    //console.log(cartItems);
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        //console.log(cartItems); bỏ console đi thay bằng
+        if(localStorage.getItem('auth-token')){
+            fetch('http://localhost:4000/addtocart',{
+                method:'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({"itemId":itemId}),
+            })
+            .then((response)=>response.json())
+            .then((data)=>console.log(data));
+        }
+    }
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: Math.max(prev[itemId] - 1, 0) }));
+        if(localStorage.getItem('auth-token')){ //kết nối với database backend
+            fetch('http://localhost:4000/removefromcart',{
+                method:'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({"itemId":itemId}),
+            })
+            .then((response)=>response.json())
+            .then((data)=>console.log(data));
+        }
+    }
+    {/*tính tổng giá các sản phẩm trong giỏ hàng */}
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if(cartItems[item] > 0)
+            {
+                let itemInfo = all_product.find((product)=>product.id===Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }   
+        }
+        return totalAmount;
+    }
+    {/*hiển thị số sản phẩm trên biểu tượng giỏ hàng */}
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for(const item in cartItems){
+            if(cartItems[item] > 0)
+            {
+                totalItem += cartItems[item];
+            }
+            
+        }
+        return totalItem;
+    }
+
+    const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
